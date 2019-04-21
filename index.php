0b830f0d5f3c7a52f7f537946254dce78939492e
Dominik <Dominik@Dominik-PCpro.mshome.net>
2019-04-21 16:16:55 +0200

    Toto je druhy commit. A vytvoření nové větve.

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
 echo ('Toto je prvni commit.');
+echo '<br>';
+$i = 8;
 
+for ($l = 0; $l < 800; $l+= $i){
+    echo $l;
+    echo '<br>';
+}
 
+echo ('Toto je druhy commit. A vytvoření nové větve.');
 ?>
